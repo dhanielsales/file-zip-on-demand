089c757e143e3094c768425928c7a3207845f4a6
Dhaniel Sales <dhanielr94@gmail.com>
2023-04-05 18:05:32 -0300

    remove unecessary comment

--- src/index.ts
+++ src/index.ts
@@ -67,24 +67,5 @@ app.get('/create-zip', async (request, response, next) => {
   });
 });
 
-// setInterval(() => {
-//   const formatMemoryUsage = (data: number) => `${Math.round((data / 1024 / 1024) * 100) / 100} MB`;
-
-//   const memoryData = process.memoryUsage();
-
-//   const memoryUsage = {
-//     rss: `${formatMemoryUsage(
-//       memoryData.rss,
-//     )} -> Resident Set Size - total memory allocated for the process execution`,
-//     heapTotal: `${formatMemoryUsage(memoryData.heapTotal)} -> total size of the allocated heap`,
-//     heapUsed: `${formatMemoryUsage(
-//       memoryData.heapUsed,
-//     )} -> actual memory used during the execution`,
-//     external: `${formatMemoryUsage(memoryData.external)} -> V8 external memory`,
-//   };
-
-//   console.log(memoryUsage);
-// }, 1000);
-
 // Start Server
 app.listen(4000, () => console.log('Server started on port 4000'));
